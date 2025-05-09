8a48c116bf9521e57ccae46a62d414077d90f3e0
Saim-Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2025-05-09 10:34:55 +0500

    Downgrade website due to prevailing cyber attacks

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,6 @@ export default function RootLayout({ children }) {
         >
         <Navbar />
         <SpeedInsights />
-        {children}
         <ScrollToTop />
         <Whatsapp />
         <ChatBot />
