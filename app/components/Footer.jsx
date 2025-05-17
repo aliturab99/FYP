3134cb7f3af2373b5c8e8c1e1d7f5f9dfe0b76b5
Saim Maqsood <maqsoodsaim2002@gmail.com>
2025-05-17 23:50:54 +0500

    Add barcode for easy access

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,10 +1,14 @@
-import Link from 'next/link';
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="bg-#1a1a1a text-white py-6"> 
       <div className="container mx-auto text-center">
         <p>&copy; {new Date().getFullYear()} medMagic. All rights reserved.</p>
+        <p>Scan to access on phone</p>
+        <center>
+        <Image src={"/frame.svg"} alt="Bar code" width={150} height={150}/>
+        </center>
         </div>
     </footer>
   );
