aa53ff1cf5bda4386f39dac7960dd7d7645b4ff7
Saim Maqsood <maqsoodsaim2002@gmail.com>
2025-05-30 13:54:29 +0500

    temporarily remove QR code

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,10 +4,6 @@ const Footer = () => {
   return (
     <footer className="bg-[#1a1a1a] text-white p-4 container mx-auto text-center"> 
       <p>&copy; {new Date().getFullYear()} medMagic. All rights reserved.</p>
-        <p>Scan to access on phone</p>
-        <center>
-        <Image src={"/frame.svg"} alt="Bar code" width={150} height={150}/>
-        </center>
     </footer>
   );
 };
