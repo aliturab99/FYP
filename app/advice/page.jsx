ea86f13ddda7c992163a6d0f8f12ec2eb48e4702
Saim Maqsood <maqsoodsaim2002@gmail.com>
2025-06-20 22:00:36 +0500

    update code

---
+++ app/advice/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { openai } from "@ai-sdk/openai"
+import { generateText } from "ai"
+const page = () => {
+  const main = async () => {
+    const { text } = await generateText({
+      model: openai("o3-mini"),
+      prompt: "Give a medical advice of how a person can keep themselves fit"
+    })
+    document.writeln(text)
+  }
+  main()
+  return (
+    <>
+    <div className='border-4 border-black'>
+      <button onClick={main}>Click me to get an advice</button>
+    </div>
+    </>
+  )
+}
+
+export default page
